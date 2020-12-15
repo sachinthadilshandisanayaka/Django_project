f1afc292ebb97ee82262250b1423253e7a56d917
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-12-15 18:16:18 +0530

    updated

--- practice_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PracticeAppConfig(AppConfig):
-    name = 'practice_app'
--- test_project/test_project/asgi.py
+++ test_project/test_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for practice_1_0 project.
+ASGI config for test_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice_1_0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
 
 application = get_asgi_application()
