747d0b51d312fe90f0bb9ba5bb4633bd99b6b42b
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-12-16 08:53:51 +0530

    updated

--- test_project/dissanayake/urls.py
+++ test_project/dissanayake/urls.py
@@ -1,7 +1,7 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path("add", views.result, name="add")
 ]
--- test_project/dissanayake/views.py
+++ test_project/dissanayake/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {'name': 'sachintha'})
+
+
+def result(request):
+    num1 = int(request.GET["num1"])
+    num2 = int(request.GET["num2"])
+    sub = num1 + num2
+    return render(request, 'result.html', {'add': sub})
