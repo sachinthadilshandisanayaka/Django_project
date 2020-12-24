18b2e52797ac48e06e39d7eaf604df20a52684f8
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-12-24 23:43:10 +0530

    added dinamic value

--- test_project/travello/views.py
+++ test_project/travello/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Destination
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {'name': 'sachintha'})
+    dest1 = Destination()
+    dest1.name = "Margarita"
+    dest1.desc = "Nothing beats the classics! With a tomato base and mozzarella cheese sprinkled with the finest " \
+                 "herbs, ypu cant say no. "
+    dest1.price = 30
+    return render(request, 'index.html', {'dest1': dest1})
