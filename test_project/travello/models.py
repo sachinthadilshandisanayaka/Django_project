057c2da6fd6fa4dbc48e28b9dba76ad4d22cc04e
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-12-26 23:35:40 +0530

    set offers

--- test_project/travello/models.py
+++ test_project/travello/models.py
@@ -8,3 +8,4 @@ class Destination:
     img: str
     desc: str
     price: int
+    offer: bool
--- test_project/travello/views.py
+++ test_project/travello/views.py
@@ -10,6 +10,7 @@ def index(request):
     dest1.img = "pizza.jpg"
     dest1.desc = "Nothing beats the classics! With a tomato base and mozzarella cheese sprinkled with the finest " \
                  "herbs, ypu cant say no. "
+    dest1.offer = False
     dest1.price = 30
 
     dest2 = Destination()
@@ -17,6 +18,7 @@ def index(request):
     dest2.img = "pizza2.jpg"
     dest2.desc = "Bacon and eggs on a pizza! Bacon, ham, mozzarella,\
                         on a tomato base, topped with egg."
+    dest2.offer = True
     dest2.price = 14
 
     dest3 = Destination()
@@ -25,6 +27,7 @@ def index(request):
     dest3.desc = "The supreme leader of pizza (no bias here). Salami, ham, chorizo, mushrooms,\
                         onions, capsicums, pineapple, on a tomato base and topped with mozzarella,\
                         herbs, and garlic. You can't go wrong."
+    dest3.offer = False
     dest3.price = 12
 
     dests = [dest1, dest2, dest3]
