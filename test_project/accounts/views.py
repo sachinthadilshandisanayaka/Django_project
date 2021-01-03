83c5a28a5e5235b7f3a43dd8712efb968f9e994c
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-01-03 18:36:03 +0530

    updated

--- test_project/accounts/views.py
+++ test_project/accounts/views.py
@@ -3,6 +3,23 @@ from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
 def register(request):
 
     if request.method == 'POST':
@@ -24,7 +41,7 @@ def register(request):
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
-                return redirect('/')
+                return redirect('login')
         else:
             messages.info(request, "Password not same")
             return redirect('register')
